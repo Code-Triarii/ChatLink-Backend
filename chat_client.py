6f8232980897f031350dd368d75ad7dc715ed125
U01A53D0 <ebustillos@proveedorexterno.com>
2023-10-05 16:17:57 +0200

    user connection notice

---
+++ chat_client.py
@@ -0,0 +1,66 @@
+import socketio
+
+from config import get_config
+
+sio = socketio.Client()
+config = get_config()
+
+def chat_print(msg):
+    print(f'CodeTriarii Chat: {msg}')
+
+def join_chat_room(data):
+    sio.emit('join_chat_room', data)
+    chat_print(f'You have successfully logged in to the room: {data["room_id"]}')
+    chat_print('Chat session started')
+@sio.event
+def joined_chat_room(data):
+    chat_print(f'Now you are in the room {data["room_id"]}')
+
+def user_connection():
+    provided_user = input('Welcome to CodeTriarii Chat, What is your name?: ')
+    chat_room = input(f'Nice to see you here, {provided_user}.\nWhich room do you want to join today?: ')
+
+    data = {
+        'user': provided_user,
+        'room_id': chat_room
+    }
+
+    sio.emit('user_connect', {
+        'user': str(provided_user)
+    })
+
+    join_chat_room(data)
+
+    return data
+
+@sio.on('receive_message')
+def receive_message(user, message, join=False):
+    if(join):
+        print(f'User {user} joined to the chat room.')
+    elif(join is False):
+        print(f'Message received by {user} {message}')
+    else:
+        print(f'User {user} left the chat room.')
+@sio.event
+def send_message(user, message, room_id):
+    sio.emit('sent_message', {'user': str(user), 'message': str(message), 'room_id': str(room_id)})
+    print(f'Message sent: {message}')
+
+@sio.event
+def disconnect():
+    chat_print('Disconnected.')
+
+sio.connect(f'ws://{config["GENERAL"]["HOST"]}:{config["GENERAL"]["PORT"]}', wait_timeout=10)
+
+user_data = user_connection()
+
+exit = False
+
+while(not exit):
+    user_input = input()
+    if(user_input == '/exit'):
+        exit = True
+        break
+    send_message(user_data['user'], user_input, user_data['room_id'])
+
+sio.disconnect()
--- sockets/user_events/room/__init__.py
+++ sockets/user_events/room/__init__.py
@@ -1,9 +1,9 @@
-from flask_socketio import join_room, leave_room, send
+from flask_socketio import join_room, leave_room, send, emit
 
 def on_join(data):
     join_room(data['room_id'])
-    send(f'{data["user"]} has joined to room {data["room_id"]}', to=data['room_id'])
+    emit('receive_message', (data["user"],'', True), to=data['room_id'], include_self=False)
     
 def on_leave(data):
     leave_room(data['room_id'])
-    send(f'{data["user"]} has left room {data["room_id"]}', to=data['room_id'])
+    emit('receive_message', (data["user"],'', None), to=data['room_id'], include_self=False)
